7735cf2e0344b2028f58f4cebb085d18b9806207
koji9412 <koji0412ohta@gmail.com>
2021-04-02 12:13:22 +0900

    test 3

--- index.tsx
+++ index.tsx
@@ -1,2 +1,28 @@
-const message: string = "Hello, world"
-console.log(message)
+/**
+ * Output: JSON Data
+ */
+ const json = fetch("https://api.github.com/users/denoland");
+
+ json.then((response) => {
+   return response.json();
+ }).then((jsonData) => {
+   console.log(jsonData);
+ });
+ 
+ /**
+  * Output: HTML Data
+  */
+ const text = fetch("https://deno.land/");
+ 
+ text.then((response) => {
+   return response.text();
+ }).then((textData) => {
+   console.log(textData);
+ });
+ 
+ /**
+  * Output: Error Message
+  */
+ const error = fetch("https://does.not.exist/");
+ 
+ error.catch((error) => console.log(error.message));
