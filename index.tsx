dd3916d1683bae311fead75ec77f7f265f960746
koji9412 <koji0412ohta@gmail.com>
2021-04-02 12:21:31 +0900

    test 3

--- index.tsx
+++ index.tsx
@@ -1,8 +1,6 @@
-import { serve, Server, ServerRequest, Response } from "https://deno.land/std/http/server.ts"
-const server: Server = serve({ port: 8000 })
-for await (const req: ServerRequest of server) {
-  const res: Response = {
-    body: "<html><body>Hello World</body></html>\n"
-  }
-  req.respond(res)
-}
+addEventListener("fetch", (event) => {
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
+});
